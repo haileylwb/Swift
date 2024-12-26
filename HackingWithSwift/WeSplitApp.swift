da9ce80ed91afbd136dfa3f8a8f1fd8bda01b837
Hailey Wong-Budiman <73897167+haileylwb@users.noreply.github.com>
2024-12-26 15:21:46 -0500

    Add files via upload

---
+++ HackingWithSwift/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Hailey Wong-Budiman on 12/24/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
